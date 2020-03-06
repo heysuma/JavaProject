da0cb9d36684ad8b40d61603f4bd8d8740ced763
Maddula Sai Lakshmi Suma <37169151+heysuma@users.noreply.github.com>
2020-03-06 14:45:44 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,33 @@
+package com.emp.bean;
+
+public class Employee {
+private int empId;
+private String name;
+private Long salary;
+private int age;
+public int getEmpId() {
+	return empId;
+}
+public void setEmpId(int empId) {
+	this.empId = empId;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public Long getSalary() {
+	return salary;
+}
+public void setSalary(Long salary) {
+	this.salary = salary;
+}
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+
+}
